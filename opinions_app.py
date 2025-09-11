19f4bc9c7397fe6392368cfda80a2f63e47611ef
Yasin <yasinzingiev@mail.ru>
2025-09-11 16:10:03 +0300

    1

--- opinions_app.py
+++ opinions_app.py
@@ -1,11 +1,30 @@
+from datetime import datetime
+
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql.expression import func
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+db = SQLAlchemy(app)
+
+
+class Opinion(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(128), nullable=False)
+    text = db.Column(db.Text, nullable=False, unique=True)
+    source = db.Column(db.String(256))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
 
 
 @app.route('/')
 def index_view():
-    return 'Совсем скоро тут будет случайное мнение о фильме!'
+    opinion = Opinion.query.order_by(func.random()).first()
+    if opinion is None:
+        return 'В базе данных мнений о фильмах нет.'
+    return opinion.text
+
 
 if __name__ == '__main__':
     app.run(debug=True)
