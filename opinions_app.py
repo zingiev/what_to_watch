7096d91101ce3f0f91fc55418837410dd73ec9ef
zingiev <m@zingiev.ru>
2025-09-14 18:22:13 +0300

    /

--- opinions_app.py
+++ opinions_app.py
@@ -5,8 +5,9 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql.expression import func
 
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+app.json.ensure_ascii = False  # type: ignore
 db = SQLAlchemy(app)
 
 
@@ -23,12 +24,12 @@ def index_view():
     opinion = Opinion.query.order_by(func.random()).first()
     if opinion is None:
         return 'В базе данных мнений о фильмах нет.'
-    return render_template('index.html', opinion=opinion)
+    return render_template('opinion.html', opinion=opinion)
 
 
 @app.route('/add')
-def add_view():
-    return render_template('add.html')
+def add_opinion_view():
+    return render_template('add_opinion.html')
 
 
 if __name__ == '__main__':
